2cdda058db8306a16af2156b8c59ee1413baf873
ahmadwaluyo <ahmad.umb15@gmail.com>
2020-10-02 22:23:32 +0700

    CRUD done

--- src/main/java/com/ngetest/crudtesting/StudentController.java
+++ src/main/java/com/ngetest/crudtesting/StudentController.java
@@ -1,9 +1,51 @@
 package com.ngetest.crudtesting;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ngetest.crudtesting.db.Student;
+import com.ngetest.crudtesting.db.StudentRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/students")
+@RequestMapping("/")
 public class StudentController {
+
+    private StudentRepository studentRepository;
+
+    public StudentController(StudentRepository studentRepository) { this.studentRepository = studentRepository; }
+
+    @GetMapping
+    public ResponseEntity<String> Hello() { return new ResponseEntity<>("Selamat datang di latihan RESTFULL API spring-boot", HttpStatus.OK);}
+
+    @GetMapping("students")
+    public ResponseEntity<?> getStudents() {
+        return new ResponseEntity<>(studentRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("students/{id}")
+    public ResponseEntity<?> getStudentById(@PathVariable Long id) {
+        Student student = studentRepository.findById(id).orElse(null);
+        if (student==null)
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(student, HttpStatus.OK);
+    }
+
+    @PostMapping("students/insert")
+    public ResponseEntity<?> postStudent(@RequestBody Student student) {
+        studentRepository.save(student);
+        return new ResponseEntity<>(student, HttpStatus.CREATED);
+    }
+
+    @PutMapping("students/{id}")
+    public ResponseEntity<?> putStudentById(@RequestBody Student student, @PathVariable Long id) {
+        student.setId(id);
+        studentRepository.save(student);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @DeleteMapping("students/{id}")
+    public ResponseEntity<?> deleteStudentById(@PathVariable Long id) {
+        studentRepository.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into student (id, name, address, phone_number) values (1, 'Ahmad Waluyo', 'Kebumen', 081271062214);
+insert into student (id, name, address, phone_number) values (2, 'Piski Pratama', 'Bandung', 08128899900);
+insert into student (id, name, address, phone_number) values (3, 'Adnin', 'Bogor', 08127556644);
